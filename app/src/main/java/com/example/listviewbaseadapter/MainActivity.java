7fb5073a6f341a11d7007b40d6aa846575cb4085
Keo Mony <keomony929@gmail.com>
2024-04-28 10:12:30 +0800

    using SimpleAdapter

--- app/src/main/java/com/example/listviewbaseadapter/MainActivity.java
+++ app/src/main/java/com/example/listviewbaseadapter/MainActivity.java
@@ -16,7 +16,6 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.listviewbaseadapter.adapter.FoodAdapter;
 import com.example.listviewbaseadapter.model.FoodDetails;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         listView = findViewById(R.id.listview);
+
         //数据列表 List Data
         List<FoodDetails> foodDetailsList = new ArrayList<>();
         for (int i = 0; i < imageIds.length; i++) {
@@ -49,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
             foodDetails.setFoodImageId(imageIds[i]);
             foodDetailsList.add(foodDetails);
         }
+
         //创建适配器 Create an Adapter
         FoodAdapter adapter = new FoodAdapter(this, foodDetailsList);
+
         //绑定适配器 Binding Adapter
         listView.setAdapter(adapter);
 
@@ -59,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // 获取点击位置的FoodDetails对象
                 FoodDetails foodDetails = foodDetailsList.get(position);
+
                 // 创建一个 Intent 来启动 FoodDetailActivity
                 Intent intent = new Intent(MainActivity.this, FoodDetailActivity.class);
                 intent.putExtra("foodDetails", (Parcelable) foodDetails);
@@ -67,3 +71,65 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
+// Using SimpleAdapter
+//public class MainActivity extends AppCompatActivity {
+//    ListView listView;
+//    private int[] imageIds = new int[]{R.drawable.food1, R.drawable.food2, R.drawable.food3, R.drawable.food2};
+//    private String[] foodNames = new String[]{"火锅糕点等中西美食", "大理美食", "重庆鸡公煲", "大理美食"};
+//    private String[] foodDesc = new String[]{"重庆鸡公煲重庆鸡公煲重庆鸡公煲重庆鸡公煲",
+//            "火锅糕点等中西美食火锅糕点等中西美食火锅糕点等中西美食 火锅糕点等中西美食",
+//            "重庆鸡公煲重庆鸡公煲重庆鸡公煲重庆鸡公煲 火锅糕点等中西美食火锅糕点等中西美食火锅糕点等中西美食 火锅糕点等中西美食",
+//            "重庆鸡公煲重庆鸡公煲重庆鸡"};
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//
+//        listView = findViewById(R.id.listview);
+//
+//        // Prepare data for SimpleAdapter
+//        List<Map<String, Object>> data = new ArrayList<>();
+//        for (int i=0; i<imageIds.length; i++){
+//            Map<String, Object> item = new HashMap<>();
+//            item.put("image", imageIds[i]);
+//            item.put("foodName", foodNames[i]);
+//            item.put("foodDesc", foodDesc[i]);
+//            data.add(item);
+//        }
+//
+//        // Define layout for each item in the list
+//        String[] from = {"image", "foodName", "foodDesc"};
+//        int[] to = {R.id.imageView, R.id.textViewName, R.id.textViewDesc};
+//
+//        // Create and Set SimpleAdapter
+//        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.foods_item, from, to);
+//        listView.setAdapter(adapter);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // Get the click FoodDetail Object
+//                Map<String, Object> item = data.get(position);
+//                int imageId = (int) item.get("image");
+//                String foodName = (String) item.get("foodName");
+//                String foodDesc = (String) item.get("foodDesc");
+//                FoodDetails foodDetails = new FoodDetails(foodName, foodDesc, imageId);
+//
+//                // Create an Intent to start FoodDetailActivity
+//                Intent intent = new Intent(MainActivity.this, FoodDetailActivity.class);
+//                intent.putExtra("foodDetails", (Parcelable) foodDetails);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
